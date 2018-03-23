aceeb83c9c465057902f97ee675ec8699143be4c
Timothy Hurt <timothyjhurt@gmail.com>
2018-03-22 21:32:13 -0700

    fixed title

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='scriboto',
+setup(name='scriboto-app',
       version='0.1',
       description='Using clinical conversations to complete EHR documentation',
       url='http://github.com/storborg/funniest',
       author='Scriboto',
       author_email='scriboto@gmail.com',
       license='MIT',
-      packages=['scriboto'],
+      packages=['scriboto-app'],
       install_requires=[
           'google-cloud-storage',
           'os',
