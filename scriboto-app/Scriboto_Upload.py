6172ee18e3cd62682edce11da4f954fc2674a4ae
Timothy Hurt <timothyjhurt@gmail.com>
2018-06-07 20:45:14 -0700

    Updating Google Cloud functions

--- scriboto-app/Scriboto_Upload.py
+++ scriboto-app/Scriboto_Upload.py
@@ -18,7 +18,7 @@ print(file_to_upload)
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
 	"""Uploads a file to the bucket."""
 	storage_client = storage.Client()
-	bucket = storage_client.get_bucket(bucket_name)
+	bucket = storage_client.bucket(bucket_name)
 	blob = bucket.blob(destination_blob_name)
 	blob.upload_from_filename(source_file_name)
 
--- scriboto-app/showResults.py
+++ scriboto-app/showResults.py
@@ -13,7 +13,7 @@ clinical_df = pd.read_csv("finding.csv")
 def download_blob(bucket_name, source_blob_name, destination_file_name):
 	"""Downloads a blob from the bucket."""
 	storage_client = storage.Client()
-	bucket = storage_client.get_bucket(bucket_name)
+	bucket = storage_client.bucket(bucket_name)
 	blob = bucket.blob(source_blob_name)
 
 	blob.download_to_filename(destination_file_name)
